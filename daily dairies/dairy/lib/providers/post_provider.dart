37fe4c2ecef503130126925e162a970d0eca3954
Maryam Fatima <mminhas1405@gmail.com>
2025-09-26 20:52:14 +0500

    Create post_provider.dart

---
+++ daily dairies/dairy/lib/providers/post_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import '../models/post.dart';
+
+class PostProvider extends ChangeNotifier {
+  final List<Post> _posts = [];
+
+  List<Post> get posts => _posts;
+
+  void addPost(Post post) {
+    _posts.insert(0, post); // 👈 latest post sabse upar
+    notifyListeners();
+  }
+}
